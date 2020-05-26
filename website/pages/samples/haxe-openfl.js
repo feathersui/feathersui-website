ce6c76806044dae0a3350674fcbc0ef5eb5b0462
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-05-26 16:16:20 -0700

    haxe/openfl samples: screenshots

--- website/pages/samples/haxe-openfl.js
+++ website/pages/samples/haxe-openfl.js
@@ -3,67 +3,89 @@ const CompLibrary = require("../../core/CompLibrary.js");
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
+const samplesBaseUrl = "/samples/haxe-openfl/";
+const githubSamplesBaseUrl =
+  "https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/";
+
+//screenshots: 720x450
+//this.stage.nativeWindow.width = 720 + (this.stage.nativeWindow.width - this.stage.stageWidth);
+//this.stage.nativeWindow.height = 450 + (this.stage.nativeWindow.height - this.stage.stageHeight);
 const sampleItems = [
   {
-    title: "[Calculator](/samples/haxe-openfl/calculator/)",
+    title: `[Calculator](${samplesBaseUrl}calculator/)`,
+    image: `${samplesBaseUrl}img/calculator.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}calculator/`,
     content: `A sample calculator application.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/calculator/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/calculator/">Source Code</a>
-</div>`
+  <a class="button" href="${samplesBaseUrl}calculator/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}calculator/">Source Code</a>
+</div>`,
   },
   {
-    title: "[Components Explorer](/samples/haxe-openfl/components-explorer/)",
+    title: `[Components Explorer](${samplesBaseUrl}components-explorer/)`,
+    image: `${samplesBaseUrl}img/components-explorer.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}components-explorer/`,
     content: `A listing of all of the UI components available in Feathers UI.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/components-explorer/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/components-explorer/">Source Code</a>
-</div>`
+  <a class="button" href="${samplesBaseUrl}components-explorer/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}components-explorer/">Source Code</a>
+</div>`,
   },
   {
-    title: "[Custom Theme](/samples/haxe-openfl/custom-theme/)",
+    title: `[Custom Theme](${samplesBaseUrl}custom-theme/)`,
+    image: `${samplesBaseUrl}img/custom-theme.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}custom-theme/`,
     content: `An example of how to create a custom theme that gives custom styles to Feathers UI components.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/custom-theme/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/custom-theme/">Source Code</a>
-</div>`
+  <a class="button" href="${samplesBaseUrl}custom-theme/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}custom-theme/">Source Code</a>
+</div>`,
   },
   {
-    title:
-      "[Percentage Sizing](/samples/haxe-openfl/horizontal-layout-percentage-sizing/)",
+    title: `[Percentage Sizing](${samplesBaseUrl}horizontal-layout-percentage-sizing/)`,
+    image: `${samplesBaseUrl}img/horizontal-layout-percentage-sizing.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}horizontal-layout-percentage-sizing/`,
     content: `Demonstrates how layouts support sizing UI component based on a percentage of their parent's size.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/horizontal-layout-percentage-sizing/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/horizontal-layout-percentage-sizing/">Source Code</a>
-</div>`
+  <a class="button" href="${samplesBaseUrl}horizontal-layout-percentage-sizing/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}horizontal-layout-percentage-sizing/">Source Code</a>
+</div>`,
   },
   {
-    title:
-      "[Collection Sorting](/samples/haxe-openfl/list-view-data-provider-collection-sorting/)",
+    title: `[Collection Sorting](${samplesBaseUrl}list-view-data-provider-collection-sorting/)`,
+    image: `${samplesBaseUrl}img/list-view-data-provider-collection-sorting.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}list-view-data-provider-collection-sorting/`,
     content: `Sort the items in a collection rendered by the Feathers UI \`ListView\` component.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/list-view-data-provider-collection-sorting/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/list-view-data-provider-collection-sorting/">Source Code</a>
-</div>`
+  <a class="button" href="${samplesBaseUrl}list-view-data-provider-collection-sorting/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}list-view-data-provider-collection-sorting/">Source Code</a>
+</div>`,
   },
   {
-    title:
-      "[Navigation Data Passing](/samples/haxe-openfl/stack-navigator-pass-data-between-views/)",
+    title: `[Navigation Data Passing](${samplesBaseUrl}stack-navigator-pass-data-between-views/)`,
+    image: `${samplesBaseUrl}img/stack-navigator-pass-data-between-views.png`,
+    imageAlign: "top",
+    imageLink: `${samplesBaseUrl}stack-navigator-pass-data-between-views/`,
     content: `When navigating between views in a \`StackNavigator\` component, data may be passed forward or back.
 
 <div class="buttonWrapper">
-  <a class="button" href="/samples/haxe-openfl/stack-navigator-pass-data-between-views/">Online Demo</a>
-  <a class="button" href="https://github.com/BowlerHatLLC/feathersui-openfl/tree/master/samples/stack-navigator-pass-data-between-views/">Source Code</a>
-</div>`
-  }
+  <a class="button" href="${samplesBaseUrl}stack-navigator-pass-data-between-views/">Online Demo</a>
+  <a class="button" href="${githubSamplesBaseUrl}stack-navigator-pass-data-between-views/">Source Code</a>
+</div>`,
+  },
 ];
 
-const Samples = props => {
+const Samples = (props) => {
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer documentContainer">
