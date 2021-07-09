c55df35b43572dc1b94982fa889bee8e760d0113
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-07-09 14:33:14 -0700

    beta.4

--- website/pages/samples/haxe-openfl.js
+++ website/pages/samples/haxe-openfl.js
@@ -5,7 +5,7 @@ const GridBlock = CompLibrary.GridBlock;
 
 const samplesBaseUrl = "/samples/haxe-openfl/";
 const githubSamplesBaseUrl =
-  "https://github.com/feathersui/feathersui-openfl/tree/v1.0.0-beta.3/samples/";
+  "https://github.com/feathersui/feathersui-openfl/tree/v1.0.0-beta.4/samples/";
 
 //screenshots: 720x450
 //this.stage.nativeWindow.width = 720 + (this.stage.nativeWindow.width - this.stage.stageWidth);
